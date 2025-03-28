94c5236d17ede6a9afe1bb7ff64bfac0cfddee28
JiaxingQiu <jiaxingq1996@gmail.com>
2025-03-28 11:14:43 -0400

    manage loss and loss printing in joint training

--- script/VITAL/train.py
+++ script/VITAL/train.py
@@ -30,8 +30,6 @@ def cross_entropy(preds, targets):
     loss = -torch.sum(targets * log_probs) / batch_size
     return loss
 
-
-
 class KLAnnealer:
     def __init__(self, start, end, epochs):
         self.start = start
@@ -51,77 +49,88 @@ def compute_vae_loss(ts, ts_hat, mean, log_var, beta=1.0):
     return vae_loss
 
   
-        
+def compute_loss(model, ts, text_features, labels, train_type='joint', alpha=1.0, beta=0.1):
+    # initialized to return
+    loss = 0
+    clip_loss = 0
+    vae_loss = 0
+
+    logits, ts_hat, mean, log_var = model(ts, text_features)
+    # compute losses
+    if train_type == 'joint':
+        clip_loss = compute_clip_loss(logits, labels)
+        vae_loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
+        loss = clip_loss + alpha * vae_loss
+    elif train_type == 'vae':
+        loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
+    elif train_type == 'clip':
+        loss = compute_clip_loss(logits, labels)
+    else:
+        raise ValueError(f"Invalid model type: {train_type}")
+    return loss, clip_loss, vae_loss
+
+
+
 def train_epoch(model, train_dataloader, optimizer, train_type='joint', alpha=1.0, beta=0.1): 
     # alpha controls the balance between vae loss and clip loss
     # beta controls the balance between reconstruction loss and kl loss
     model.train()
     total_loss = 0
+    total_clip_loss = 0 # only updated for joint training
+    total_vae_loss = 0 # only updated for joint training
     num_batches = 0
     
-    for batch_idx, (ts, text_features, labels) in enumerate(train_dataloader):
+    for _, (ts, text_features, labels) in enumerate(train_dataloader):
         
+        loss, clip_loss, vae_loss = compute_loss(model, ts, text_features, labels, train_type, alpha, beta)
+
         # Clear gradients
         optimizer.zero_grad()
-
-        # forward pass  
-        logits, ts_hat, mean, log_var = model(ts, text_features)
-
-        # compute losses
-        if train_type == 'joint':
-            clip_loss = compute_clip_loss(logits, labels)
-            vae_loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
-            loss = clip_loss + alpha * vae_loss
-        elif train_type == 'vae':
-            loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
-        elif train_type == 'clip':
-            loss = compute_clip_loss(logits, labels)
-        else:
-            raise ValueError(f"Invalid model type: {train_type}")
-        
         # backward pass
         loss.backward(retain_graph=True)
-
         # Add gradient clipping
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        
         # Update weights
         optimizer.step()
 
         # update total loss and number of batches
         total_loss += loss.item()
+        if train_type == 'joint':
+            total_clip_loss += clip_loss.item()
+            total_vae_loss += vae_loss.item()
+        
         num_batches += 1
     
-    return total_loss / num_batches
+    avg_loss = total_loss / num_batches
+    avg_clip_loss = total_clip_loss / num_batches
+    avg_vae_loss = total_vae_loss / num_batches
+    
+    return avg_loss, avg_clip_loss, avg_vae_loss
+
+
 
 def test_epoch(model, test_dataloader, train_type='joint', alpha=1.0, beta=0.1):
     model.eval()
     total_loss = 0
+    total_clip_loss = 0 # only updated for joint training
+    total_vae_loss = 0 # only updated for joint training
     num_batches = 0
     
     with torch.no_grad():
         for batch_idx, (ts, text_features, labels) in enumerate(test_dataloader):
-            
-            logits, ts_hat, mean, log_var = model(ts, text_features)
-            
-            # compute losses
-            if train_type == 'joint':
-                clip_loss = compute_clip_loss(logits, labels)
-                print(f'batch {batch_idx} test clip_loss: {clip_loss.item()}')
-                vae_loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
-                print(f'batch {batch_idx} test vae_loss: {vae_loss.item()}')
-                loss = clip_loss + alpha * vae_loss
-            elif train_type == 'vae':
-                loss = compute_vae_loss(ts, ts_hat, mean, log_var, beta)
-            elif train_type == 'clip':
-                loss = compute_clip_loss(logits, labels)
-            else:
-                raise ValueError(f"Invalid model type: {train_type}")
+            loss, clip_loss, vae_loss = compute_loss(model, ts, text_features, labels, train_type, alpha, beta)
             
             total_loss += loss.item()
+            if train_type == 'joint':
+                total_clip_loss += clip_loss.item()
+                total_vae_loss += vae_loss.item()
             num_batches += 1
     
-    return total_loss / num_batches
+    avg_loss = total_loss / num_batches
+    avg_clip_loss = total_clip_loss / num_batches
+    avg_vae_loss = total_vae_loss / num_batches
+    
+    return avg_loss, avg_clip_loss, avg_vae_loss
 
 
 def train_vital(model, train_dataloader, test_dataloader, optimizer, scheduler, kl_annealer, num_epochs, train_type='joint', alpha=1.0):
@@ -134,21 +143,14 @@ def train_vital(model, train_dataloader, test_dataloader, optimizer, scheduler, 
     best_model_state = None
     try:
         for epoch in range(num_epochs):
-            # if epoch < int(0.2 * num_epochs): # first 20% of num_epochs
-            #     scheduler.patience = min(int(0.1*num_epochs), 50)
-            # elif epoch < int(0.5 * num_epochs): # second 30% of num_epochs
-            #     scheduler.patience = min(int(0.05*num_epochs), 30)
-            # else:
-            #     scheduler.patience = min(int(0.01*num_epochs), 10)
-            
 
             beta = kl_annealer.get_beta(epoch)
 
             # Train for one epoch
-            train_loss = train_epoch(model, train_dataloader, optimizer, train_type, alpha, beta)
+            train_loss, train_clip_loss, train_vae_loss = train_epoch(model, train_dataloader, optimizer, train_type, alpha, beta)
             
             # Test for one epoch
-            test_loss = test_epoch(model, test_dataloader, train_type, alpha, beta)
+            test_loss, test_clip_loss, test_vae_loss = test_epoch(model, test_dataloader, train_type, alpha, beta)
             
             # Store losses
             train_losses.append(train_loss)
@@ -169,11 +171,18 @@ def train_vital(model, train_dataloader, test_dataloader, optimizer, scheduler, 
 
             # Print progress
             if epoch % 10 == 0:
-                print(f'Epoch [{epoch+1}/{num_epochs}]')
-                print(f'\tTraining Loss: {train_loss:.6f}')
-                print(f'\tTesting Loss: {test_loss:.6f}')
-                print(f'\tLearning Rate: {current_lr:.9f}')
-                print(f'beta: {beta}')
+                if train_type == 'joint':
+                    print(f'Epoch [{epoch+1}/{num_epochs}]')
+                    print(f'\tTraining Loss: {train_loss:.6f} (clip: {train_clip_loss:.6f}, vae: {train_vae_loss:.6f})')
+                    print(f'\tTesting Loss: {test_loss:.6f} (clip: {test_clip_loss:.6f}, vae: {test_vae_loss:.6f})')
+                    print(f'\tLearning Rate: {current_lr:.9f}')
+                    print(f'beta: {beta}')
+                else:
+                    print(f'Epoch [{epoch+1}/{num_epochs}]')
+                    print(f'\tTraining Loss: {train_loss:.6f}')
+                    print(f'\tTesting Loss: {test_loss:.6f}')
+                    print(f'\tLearning Rate: {current_lr:.9f}')
+                    print(f'beta: {beta}')
             
             # Early stopping if learning rate becomes too small
             if current_lr <= scheduler.min_lrs[0]:
